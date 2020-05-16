e455c0a570acfc6f4a22168458af4361c28835a5
Glenn Salaman <g.salaman@comcast.net>
2020-05-16 09:20:00 -0600

    First cut...untested

--- stick_rot_test.ino
+++ stick_rot_test.ino
@@ -3,7 +3,9 @@
  *  
  * Test code for the tiny-stick board.  
  * Uses a reed switch instead of a button.
- * First test:  Lights go red when near magnet.  Green when away.
+ * Rotation period test:
+ *   Calculate how many ms per rotation.
+ *   Display on the LEDs (yay binary!)
  */
 
 #include <Adafruit_NeoPixel.h>
@@ -24,8 +26,6 @@ Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB+NEO_KHZ
 #define COLOR_BLACK   0
 #define COLOR_WHITE   0xFFFFFF
 
-
-
 /*================================================================================
  * fillAll
  */
@@ -37,9 +37,85 @@ void fillAll( uint32_t color )
   {
     pixels.setPixelColor(i, color);
   }
+  pixels.show();
 }
 
+/* get_switch_state
+ *  Initially, we're just going to use this as a passthrough, but if we
+ *  need to debounce, we'll do so in this funciton.
+ */
+int get_switch_state( void )
+{
+  return(digitalRead(BUTTON_PIN));
+}
 
+//==============================================================================================
+// FUNCTION:  write_and_latch_byte
+//
+// Writes an entire byte to the RGB LEDs.  Note this function
+// goes MSB first.
+//==============================================================================================
+#define BITS_IN_BYTE 8
+void write_and_latch_byte( int data, uint32_t color )
+{
+  int temp_bit;
+  int temp_data;
+  int i;
+
+  // Make sure it's only one byte of data...mask off the rest.
+  data &= 0xFF;
+    
+  temp_data = data;
+  
+  for (i = 0; i < BITS_IN_BYTE; i++)
+  {
+    // we only want the MSB
+    temp_bit = temp_data & 0x80;
+    temp_bit = temp_bit >> 7;
+
+    if (temp_bit) pixels.setPixelColor(i, color);
+    else pixels.setPixelColor(i, 0x00);
+    
+    // now shift our byte to get the next bit
+    temp_data = temp_data << 1;     
+  }
+
+  pixels.show();
+
+}
+
+//==============================================================================================
+// FUNCTION:  write_and_latch_byte_lsb_first
+//
+// Writes an entire byte to the rgb LEDs.  Note this function
+// goes LSB first.
+//==============================================================================================
+void write_and_latch_byte_lsb_first( int data, uint32_t color)
+{
+  int temp_bit;
+  int temp_data;
+  int i;
+
+  // Make sure we're just grabbing one byte
+  data &= 0xFF;
+    
+  temp_data = data;
+  
+  for (i = 0; i < BITS_IN_BYTE; i++)
+  {
+    // we only want the LSB
+    temp_bit = temp_data & 0x01;
+
+    if (temp_bit) pixels.setPixelColor(i, color);
+    else pixels.setPixelColor(i, 0x00);
+    
+    // now shift our byte to get the next bit
+    temp_data = temp_data >> 1;     
+  }
+
+  pixels.show();
+  
+}
 
 
 void setup()
@@ -61,26 +137,52 @@ void setup()
       delay(50);
       pixels.show();
     }
-    delay(100);
-    
-    pixels.show();
     delay(1000);
+
 }
 
 void loop()
 {
+  static uint32_t last_switch_ms=0;
+  static int      last_switch_pos=HIGH;
+  uint32_t current_ms;
+  static uint32_t last_rot_ms=10000;
+  int      current_switch_pos;
   uint32_t color;
+  uint32_t rps;
   
-  if (digitalRead(BUTTON_PIN) == HIGH)
+  current_ms = millis();
+  current_switch_pos = get_switch_state();
+  
+  /* if we just switched to low, flag this as the start of a rotation */
+  if ((last_switch_pos == HIGH) && (current_switch_pos == LOW))
   {
-    color = COLOR_RED;
+    last_rot_ms = current_ms - last_switch_ms;
+    last_switch_ms = current_ms;
+  }
+
+  last_switch_pos = current_switch_pos;
+
+  /* Display how many MS/Rotation on the LED screen. 
+   *  Note...this number *should* be in the hundreds of ms range.
+   *  If it's bigger than 2000 ms, make all the lights yellow.  We're stopped.
+   *  If it's smaller than 50, make all the lights red.  Debounce issue.
+   *  Otherwise, lets calculate rotations per second and display that.
+   */
+  if (last_rot_ms > 2000) 
+  {
+    // consider this stopped.
+    fillAll(COLOR_YELLOW); 
+  }
+  else if (last_rot_ms < 50)
+  {
+    //bounce occured....we're too fast.
+    fillAll(COLOR_RED);
   }
   else
   {
-    color = COLOR_GREEN;
+    rps = 1000 / last_rot_ms;
+    write_and_latch_byte(rps, COLOR_GREEN);
   }
-  
-  fillAll(color);
-  pixels.show();
 
 }
